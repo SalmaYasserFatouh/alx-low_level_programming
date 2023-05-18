e7005e2fe62fb0947f8bb492a2597c963829c452
SalmaYasserFatouh <salmaakamees45@gmail.com>
2023-05-17 17:55:10 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "maim.h"
+
+/**
+ * reset_to_98 - takes a pointet to an int parameter
+ *		and updates the value it points to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
